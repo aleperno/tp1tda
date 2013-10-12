c8c5e22bca916f56b26a44750c9c8a6869788cba
aleperno <ale.pernin@gmail.com>
2013-10-12 04:50:14 -0300

    Added test2.py

--- test1.py
+++ test1.py
@@ -2,38 +2,7 @@
 import sys
 from Graph import *
 
-def usage():
-	print "Usage: python test.py <filename>"
-	print "Usage: ./test.py <filename>"
-
-def loadGraph():
-	graph = Graph()
-	try:
-		aux = open(sys.argv[1])
-		for line in aux:
-			line = line.replace(':',',').replace('\n','').split(',')
-			v1 = Vertex(line[0])
-			graph.addVertex(v1)
-			for n in range(1,len(line)):
-				v2 = Vertex(line[n])
-				if (not graph.isVertex(v2)):
-					graph.addVertex(v2)
-				e = Edge(v1,v2)
-				graph.addEdge(e)
-		return graph
-
-	except KeyError:
-		print "An error has occurred"
-	except IndexError:
-		usage()
-	except IOError:
-		print "Verify parameters"
-
-
 def main():
-	g = loadGraph()
-	print g
-"""
 	v1 = Vertex('A')
 	v2 = Vertex('B')
 	v3 = Vertex('C')
@@ -53,6 +22,6 @@ def main():
 	v4 = Vertex('A')
 	g.delVertex(v4)
 	print g
-"""
+
 if __name__ == '__main__':
 	main()
